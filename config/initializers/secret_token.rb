89d3f309b49cf7ad81fbd02322a9497efb997e76
Cabranet <tom@cabranet.com>
2012-09-24 19:26:23 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlchemyGallery::Application.config.secret_token = '56c38a86efcedeef36040cbdfa87ed0f7b0095ef744c2dbdd23c4baf69ddac28b62167b3c8b2590d91b158266c1b820f3f75fc248ccab5a36bc8c0924f578fd8'
