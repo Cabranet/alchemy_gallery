19b1d7a52f34e6093ea1349e79dfe05e11284867
Cabranet <tom@cabranet.com>
2012-09-26 13:06:20 +0200

    Added a site layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,117 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+/*    Cabranet JavaScript/jQuery
+    Copyright Cabranet Limited
+    2012
+*/
+
+// Function for changing Indicator divs to reflect the current slide
+function changeIndicator($slideCounter) {
+  if ($slideCounter==1) {
+    $('.show1').css ('background-color','#FF0');
+    $('.show2,.show3').css ('background-color','#A6BDD6');
+  }
+  if ($slideCounter==2) {
+    $('.show2').css ('background-color','#FF0');
+    $('.show1,.show3').css ('background-color','#A6BDD6');
+  }
+  if ($slideCounter==3) {
+    $('.show3').css ('background-color','#FF0');
+    $('.show1,.show2').css ('background-color','#A6BDD6');
+  }
+}
+
+// Changing slide function
+function changeSlide ($slideCounter) {
+  if ($('.scroll-bar').children().index('.scroll'+$slideCounter)!=0) {
+    $('.scroll'+$slideCounter).insertBefore('.scroll-bar span:first');
+  }
+}
+
+// Set slide counter to 1
+$slideCounter = 1;
+    
+// Wait for document to be ready
+$(document).ready( function() {
+  
+  // Set up the coloured slide indicators
+  changeIndicator($slideCounter);
+  changeSlide($slideCounter);
+  
+  // When next clicked move the first slide to after the last slide
+  $('#next').click (function() {
+    if ($slideCounter!=3) {
+      $slideCounter += 1
+    }
+    else {
+      $slideCounter = 1
+    }
+    changeSlide($slideCounter);
+    changeIndicator($slideCounter);
+  });
+  
+  //When previous clicked move the last slide to before the first slide
+  $('#prev').click (function() {
+    if ($slideCounter!=1) {
+      $slideCounter -= 1
+    }
+    else {
+      $slideCounter = 3
+    }
+    changeSlide($slideCounter);
+    changeIndicator($slideCounter);
+  });
+  
+  //Show arrow on hover
+  $('#next').mouseover(function () {
+    $(this).css ('background-image','url(images/arrow_right.png)');
+    $(this).animate({right: '-=10'}, 400);
+    $(this).animate({right: '+=10'}, 400);
+  });
+  $('#next').mouseout(function () {
+    $(this).css ('background-image','none');
+  });
+  
+  //When previous clicked move the last slide to before the first slide
+  $('#prev').mouseover(function () {
+    $(this).css ('background-image','url(images/arrow_left.png)');
+    $(this).animate({left: '-=10'}, 400);
+    $(this).animate({left: '+=10'}, 400);
+  });
+  $('#prev').mouseout(function () {
+    $(this).css ('background-image','none');
+  });
+  
+  //Go to slide 1 when .show1 clicked
+  $('.show1').click (function () {
+    $slideCounter = 1;
+    changeSlide($slideCounter);
+    changeIndicator($slideCounter);
+  });
+  //Go to slide 2 when .show2 clicked
+  $('.show2').click (function () {
+    $slideCounter = 2;
+    changeSlide($slideCounter);
+    changeIndicator($slideCounter);
+  });
+  //Go to slide 3 when .show3 clicked
+  $('.show3').click (function () {
+    $slideCounter = 3;
+    changeSlide($slideCounter);
+    changeIndicator($slideCounter);
+  });
+});
+
+$(window).load(function() { //start after HTML, images have loaded
+  setInterval(function(){
+    if ($slideCounter!=3) {
+      $slideCounter += 1
+    }
+    else {
+      $slideCounter = 1
+    }
+    changeSlide($slideCounter);
+    changeIndicator($slideCounter);
+  }, 5500);
+});
